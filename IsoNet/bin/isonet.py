fix bug in rotation noise

IsoNet/bin/isonet.py
@@ -379,7 +379,10 @@ class ISONET:
 
         def normalize_and_predict(network_model, tomo_name, F_mask=None):
             tomo, _ = read_mrc(tomo_name)
-            tomo = normalize(tomo*-1,percentile=False)
+            Z = tomo.shape[0]
+            mean = np.mean(tomo.data[Z//2-16:Z//2+16])
+            std = np.std(tomo.data[Z//2-16:Z//2+16])
+            tomo = (mean-tomo)/std
             outData = network_model.predict_map(tomo, output_dir, cube_size=inner_cube_size, crop_size=cube_size, \
                                           F_mask=F_mask)
             if len(outData) == 2:
@@ -640,9 +643,7 @@ class ISONET:
         t2, _ = read_mrc(t2)
         b1, _ = read_mrc(b1)
         b2, _ = read_mrc(b2)
-        mean = (t1+t2+b1+b2)/4
-        std = np.std([t1,t2,b1,b2], axis=0)
-        write_mrc("std_map.mrc",std)
+
         shape = t1.shape
         mask_top = np.zeros_like(t1)
         mask_top[:,:shape[1]//2,:] = 1
@@ -650,11 +651,11 @@ class ISONET:
 
         half1 = t1*mask_bottom+b1*mask_top
         half2 = t2*mask_bottom+b2*mask_top
-        write_mrc("half1.mrc",-half1)
-        write_mrc("half2.mrc",-half2)
 
-
+        from IsoNet.utils.processing import FSC
+        print(FSC(half1,half2))
         return 0
+    
     def powerlaw_filtering(self, 
                     h1: str,
                     o: str = "weighting.mrc",

IsoNet/models/train.py
@@ -26,7 +26,7 @@ def apply_F_filter_torch(input_map,F_map):
     fft_input = torch.fft.fftn(input_map, dim=(-1, -2, -3))
     mw_shift = torch.fft.fftshift(F_map, dim=(-1, -2, -3))
     out = torch.fft.ifftn(mw_shift*fft_input,dim=(-1, -2, -3))
-    out =  np.real(out).real
+    out =  torch.real(out)
     return out
 
 def ddp_train(rank, world_size, port_number, model, train_dataset, training_params):
@@ -120,11 +120,8 @@ def ddp_train(rank, world_size, port_number, model, train_dataset, training_para
                         rotate_func = rotate_vol
                         rot = random.choice(rotation_list)
 
-                    if training_params['method'] == "isonet2" and training_params["noise_level"] > 0:
-                        noise_vol = apply_F_filter_torch(noise_vol, mw)
-                        x1 += noise_vol * training_params["noise_level"] / torch.std(noise_vol) * random.random()
-
                     std_org, mean_org = x1.std(), x1.mean()
+
 
                     # TODO whether need to apply wedge to x1
                     with torch.no_grad():
@@ -149,7 +146,14 @@ def ddp_train(rank, world_size, port_number, model, train_dataset, training_para
                     # This normalization need to be tested
                     mw_rotated_subtomos = (mw_rotated_subtomos - mw_rotated_subtomos.mean())/mw_rotated_subtomos.std() \
                                                 *std_org + mean_org
+                    # print(mean_org,subtomos.mean(), rotated_subtomo.mean(), mw_rotated_subtomos.mean())
+                    # print(std_org,subtomos.std(),rotated_subtomo.std(),  mw_rotated_subtomos.std())
+
                     
+                    if training_params['method'] == "isonet2" and training_params["noise_level"] > 0:
+                        noise_vol = apply_F_filter_torch(noise_vol, mw)
+                        mw_rotated_subtomos += noise_vol * training_params["noise_level"] / torch.std(noise_vol)# * random.random()
+
                     with torch.autocast('cuda', enabled = training_params["mixed_precision"]): 
                         pred_y = model(mw_rotated_subtomos).to(torch.float32)
                         outside_mw_loss, inside_mw_loss = masked_loss(pred_y, x2_rot, rotated_mw, mw, loss_func = loss_func)