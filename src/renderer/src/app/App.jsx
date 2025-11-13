1f1b8aa0785498b515216af6ec5df29e6924ca00
xyzhang <zxy5vieve@gmail.com>
2025-11-13 00:08:57 -0800

    add menu mapping

--- src/renderer/src/app/App.jsx
+++ src/renderer/src/app/App.jsx
@@ -1,5 +1,8 @@
 import './index.css'
 import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react'
+import PageCommon from '../components/PageCommon'
+import theme from '../theme.js'
+import { mergeMsg, processMessage } from '../utils/utils.js'
 import {
     List,
     ListItem,
@@ -21,93 +24,7 @@ import { keyframes } from '@mui/system'
 import { alpha } from '@mui/material/styles'
 import EditIcon from '@mui/icons-material/Edit'
 import Backdrop from '@mui/material/Backdrop'
-import DrawerPrepare from '../components/Drawer/DrawerPrepare.jsx'
-import DrawerRefine from '../components/Drawer/DrawerRefine.jsx'
-import DrawerDenoise from '../components/Drawer/DrawerDenoise.jsx'
-import DrawerPredict from '../components/Drawer/DrawerPredict.jsx'
-import DrawerDeconv from '../components/Drawer/DrawerDeconv.jsx'
-import DrawerMask from '../components/Drawer/DrawerMask.jsx'
-import PagePrepare from '../components/PagePrepare'
-import PageCommon from '../components/PageCommon'
-import PageJobs from '../components/PageJobViewer'
-import PageSettings from '../components/PageSettings'
-import DocumentationPage from '../components/PageDocs'
-import theme from '../theme.js'
-import { mergeMsg, processMessage } from '../utils/utils.js'
-import SourceTwoToneIcon from '@mui/icons-material/SourceTwoTone';
-import GraphicEqTwoToneIcon from '@mui/icons-material/GraphicEqTwoTone';
-import MasksTwoToneIcon from '@mui/icons-material/MasksTwoTone';
-import FaceRetouchingNaturalTwoToneIcon from '@mui/icons-material/FaceRetouchingNaturalTwoTone';
-import FilterHdrTwoToneIcon from '@mui/icons-material/FilterHdrTwoTone';
-import AlbumTwoToneIcon from '@mui/icons-material/AlbumTwoTone';
-import BallotTwoToneIcon from '@mui/icons-material/BallotTwoTone';
-import DescriptionIcon from '@mui/icons-material/Description';
-import SettingsIcon from '@mui/icons-material/Settings';
-
-const primaryMenuListinOrder = [
-    'prepare_star',
-    'denoise',
-    'deconv',
-    'make_mask',
-    'refine',
-    'predict',
-    'jobs_viewer',
-    'documents',
-    'settings'
-]
-const primaryMenuMapping = {
-    prepare_star: {
-        label: 'Prepare',
-        drawer: DrawerPrepare,
-        page: PagePrepare,
-        icon: SourceTwoToneIcon,
-    },
-    denoise: {
-        label: 'Denoise',
-        drawer: DrawerDenoise,
-        page: PageCommon,
-        icon: GraphicEqTwoToneIcon,
-    },
-    deconv: {
-        label: 'Deconvolve',
-        drawer: DrawerDeconv,
-        page: PageCommon,
-        icon: AlbumTwoToneIcon,
-    },
-    make_mask: {
-        label: 'Create Mask',
-        drawer: DrawerMask,
-        page: PageCommon,
-        icon: MasksTwoToneIcon,
-    },
-    refine: {
-        label: 'Refine',
-        drawer: DrawerRefine,
-        page: PageCommon,
-        icon: FaceRetouchingNaturalTwoToneIcon,
-    },
-    predict: {
-        label: 'Predict',
-        drawer: DrawerPredict,
-        page: PageCommon,
-        icon: FilterHdrTwoToneIcon,
-    },
-    jobs_viewer: {
-        label: 'Jobs Viewer',
-        page: PageJobs,
-        icon: BallotTwoToneIcon,
-    },
-    documents: {
-        label: 'Documents',
-        page: DocumentationPage,
-        icon: DescriptionIcon,
-    },
-    settings: {
-        label: 'Settings',
-        page: PageSettings,
-        icon: SettingsIcon,
-    }
-}
+import { primaryMenuListinOrder, primaryMenuMapping } from './menuMapping.js'
 
 const App = () => {
     const [blocking, setBlocking] = useState(false)
